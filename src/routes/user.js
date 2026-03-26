46f3af4407315ba61faa31b40a119bc6a13e00bd
Abhijoy Samaddar <samaddar.abhijoy@gmail.com>
2026-03-26 18:26:06 +0530

    Feed API, Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
@@ -50,7 +51,32 @@ userRouter.get("/user/connections", userAuth, async(req, res) => {
 
 userRouter.get("/feed", userAuth, async(req, res) => {
     try{
-        
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [{fromUserId: loggedInUser._id}, {toUserId: loggedInUser._id}]
+        }).select("fromUserId toUserId");
+        //}).select("fromUserId toUserId").populate("fromUserId", "firstName lastName").populate("toUserId", "firstName lastName");
+
+        //Set is a data structure which only stores unique values, it does not allow duplicates, it has very fast lookup time for checking if a value exists in the set or not.
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req)=>{
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+        //console.log(hideUsersFromFeed);
+        const users = await User.find({
+            $and: [ 
+                {_id: {$nin: Array.from(hideUsersFromFeed)}},
+                {_id: {$ne: loggedInUser._id}}, 
+            ],
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+        //console.log(users);
+        res.json({data: users});
     } catch(err) {
         res.status(400).send("ERROR: " + err.message);
     }
